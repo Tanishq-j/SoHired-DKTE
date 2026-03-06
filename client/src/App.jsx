d8e968fb374e7826428d053c93f5e6cbad4925a3
Shravani Maralkar <shravanimaralkar11@gmail.com>
2026-03-07 01:59:25 +0530

    feat: Enhance App component with new routes for Accepted Jobs, Rejected Jobs, and Resume Builder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,65 +10,86 @@ import JobPreferences from "./pages/Settings/JobPreferences";
 import CourseSuggestions from "./pages/Careers/CourseSuggestions";
 import Roadmaps from "./pages/Careers/Roadmaps";
 import ATSScanner from "./pages/ResumeAI/ATSScanner";
+import ResumeBuilder from "./pages/ResumeAI/ResumeBuilder";
+import AcceptedJobs from "./pages/Careers/AcceptedJobs";
+import RejectedJobs from "./pages/Careers/RejectedJobs";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/onboarding" element={
-        <ProtectedRoute>
-          <Onboarding />
-        </ProtectedRoute>
-      } />
+    return (
+        <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/onboarding" element={
+                <ProtectedRoute>
+                    <Onboarding />
+                </ProtectedRoute>
+            } />
 
-      <Route path="/dashboard" element={<DashboardLayout />}>
-        <Route index element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
-
-      </Route>
-      <Route path="my-profile" element={
-        <ProtectedRoute>
-          <MyProfile />
-        </ProtectedRoute>
-      } />
-
-      <Route path="my-account" element={
-        <ProtectedRoute>
-          <MyAccount />
-        </ProtectedRoute>
-      } />
-
-      <Route path="job-preferences" element={
-        <ProtectedRoute>
-          <JobPreferences />
-        </ProtectedRoute>
-      } />
-
-      <Route path="roadmaps" element={
-        <ProtectedRoute>
-          <Roadmaps />
-        </ProtectedRoute>
-      } />
-      <Route path="course-suggestions" element={
+            <Route path="/dashboard" element={<DashboardLayout />}>
+                <Route index element={
                     <ProtectedRoute>
-                        <CourseSuggestions />
+                        <Dashboard />
                     </ProtectedRoute>
                 } />
-      <Route path="roadmaps" element={
-                    <ProtectedRoute>
-                        <Roadmaps />
-                    </ProtectedRoute>
-                } /> 
-      <Route path="ats-scanner" element={
-                    <ProtectedRoute>
-                        <ATSScanner />
-                    </ProtectedRoute>
-                } />      
-    </Routes>
-  );
+
+            </Route>
+            <Route path="my-profile" element={
+                <ProtectedRoute>
+                    <MyProfile />
+                </ProtectedRoute>
+            } />
+
+            <Route path="my-account" element={
+                <ProtectedRoute>
+                    <MyAccount />
+                </ProtectedRoute>
+            } />
+
+            <Route path="job-preferences" element={
+                <ProtectedRoute>
+                    <JobPreferences />
+                </ProtectedRoute>
+            } />
+
+            <Route path="roadmaps" element={
+                <ProtectedRoute>
+                    <Roadmaps />
+                </ProtectedRoute>
+            } />
+            <Route path="course-suggestions" element={
+                <ProtectedRoute>
+                    <CourseSuggestions />
+                </ProtectedRoute>
+            } />
+            <Route path="roadmaps" element={
+                <ProtectedRoute>
+                    <Roadmaps />
+                </ProtectedRoute>
+            } />
+            <Route path="ats-scanner" element={
+                <ProtectedRoute>
+                    <ATSScanner />
+                </ProtectedRoute>
+            } />
+
+            <Route path="accepted-jobs" element={
+                <ProtectedRoute>
+                    <AcceptedJobs />
+                </ProtectedRoute>
+            } />
+            <Route path="rejected-jobs" element={
+                <ProtectedRoute>
+                    <RejectedJobs />
+                </ProtectedRoute>
+            } />
+
+            <Route path="resume-builder" element={
+                <ProtectedRoute>
+                    <ResumeBuilder />
+                </ProtectedRoute>
+            } />
+
+        </Routes>
+    );
 };
 
 
